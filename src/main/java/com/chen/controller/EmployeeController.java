5e9f110bcf66951f5554772ccf18a503405ea468
cbb <470557347@qq.com>
2022-07-14 11:44:08 +0800

    add employee management function

---
+++ src/main/java/com/chen/controller/EmployeeController.java
@@ -0,0 +1,24 @@
+package com.chen.controller;
+
+import com.chen.dao.EmployeeDao;
+import com.chen.pojo.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+
+    @Autowired
+    EmployeeDao employeeDao;
+
+    @RequestMapping("/user/emps")
+    public String list(Model model){
+        Collection<Employee> employees = employeeDao.getAll();
+        model.addAttribute("emps", employees);
+        return "emp/list";
+    }
+}
