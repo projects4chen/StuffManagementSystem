2f1eb2936d1be50a88ce24e6d5cfe1f2b2958ff1
cbb <470557347@qq.com>
2022-07-14 16:02:13 +0800

    add logout function

--- src/main/java/com/chen/controller/LoginController.java
+++ src/main/java/com/chen/controller/LoginController.java
@@ -26,4 +26,10 @@ public class LoginController {
             return "index";
         }
     }
+
+    @RequestMapping("/user/logout")
+    public String logout(HttpSession session){
+        session.invalidate();
+        return "redirect:/index.html";
+    }
 }
